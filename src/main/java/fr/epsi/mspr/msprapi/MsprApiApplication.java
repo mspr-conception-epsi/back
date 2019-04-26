99f9eda0ade2d0948395041ec29e7299efe30dcd
unknown <shyndard@gmail.com>
2019-04-26 11:59:25 +0200

    first commit

---
+++ src/main/java/fr/epsi/mspr/msprapi/MsprApiApplication.java
@@ -0,0 +1,13 @@
+package fr.epsi.mspr.msprapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsprApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsprApiApplication.class, args);
+	}
+
+}
