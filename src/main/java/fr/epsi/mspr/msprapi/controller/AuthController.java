a657d38e6723d9b4f5149eb1e2607db099271c36
Mael <shyndard@gmail.com>
2019-05-16 09:36:28 +0200

    tentative ajout password encoder

--- src/main/java/fr/epsi/mspr/msprapi/controller/AuthController.java
+++ src/main/java/fr/epsi/mspr/msprapi/controller/AuthController.java
@@ -4,19 +4,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.epsi.mspr.msprapi.repository.UserRepository;
-import fr.epsi.mspr.msprapi.security.JwTAuthentificationRequest;
 import fr.epsi.mspr.msprapi.security.JwtTokenProvider;
 
 @RestController
@@ -30,19 +29,18 @@ public class AuthController {
     JwtTokenProvider jwtTokenProvider;
 
     @Autowired
-    UserRepository users;
+    UserRepository userRepository;
 
-    @PostMapping("/signin")
-    public ResponseEntity<?> signin(@RequestBody JwTAuthentificationRequest data) {
+    @PostMapping("/signin/{user}/{password}")
+    public Map<Object, Object> signin(@PathVariable("user") String username, @PathVariable("password") String password) {
         try {
-            String username = data.getUsername();
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, data.getPassword()));
-            String token = jwtTokenProvider.createToken(username, this.users.findByName(username).orElseThrow(() -> new UsernameNotFoundException("Username " + username + "not found")).getRoles());
-
+        	password = PasswordEncoderFactories.createDelegatingPasswordEncoder().encode(password);
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+            String token = jwtTokenProvider.createToken(username, this.userRepository.findByName(username).orElseThrow(() -> new UsernameNotFoundException("Username " + username + "not found")).getRoles());
             Map<Object, Object> model = new HashMap<>();
             model.put("username", username);
             model.put("token", token);
-            return ResponseEntity.ok(model);
+            return model;
         } catch (AuthenticationException e) {
             throw new BadCredentialsException("Invalid username/password supplied");
         }
--- src/main/java/fr/epsi/mspr/msprapi/security/SecurityConfig.java
+++ src/main/java/fr/epsi/mspr/msprapi/security/SecurityConfig.java
@@ -5,9 +5,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -30,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
                 .authorizeRequests()
-                	.antMatchers(HttpMethod.POST, "/auth/signin").permitAll()
+                	.antMatchers("/auth/**").permitAll()
                 	.antMatchers("/v2/api-docs").permitAll()
                 	.antMatchers("/swagger-resources/**").permitAll()
                 	.antMatchers("/swagger-ui.html").permitAll()
